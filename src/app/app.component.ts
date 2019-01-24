058cfa33968d5abf4be0f852610da0836a4272e4
Sharis <sarezas@gmail.com>
2019-01-24 19:19:58 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  animations : [
+    // a simple trigger
+    trigger('animateDiv', [
+      state('normal', style({
+        'border-radius': '0',
+        'background-color': 'orangered',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'border-radius': '50px',
+        'background-color': 'blue',
+        transform: 'translateX(1040px)'
+      })),
+      transition('normal <=> highlighted', animate(1000)),
+      // transition('normal => highlighted', animate(300)),
+      // transition('highlighted => normal', animate(700))
+    ]),
+    // a wild trigger
+    trigger('animateDivWild', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(1040px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          borderRadius: '0',
+          'background-color': 'green'
+        }),
+        animate(1000,
+          style({borderRadius: '50px'}
+        )),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        'background-color': 'orangered',
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'animations';
+  // in ngAnimations you transition between two states, thats the condition for the animations to work in the template
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+      this.list.push(item);
+    }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
